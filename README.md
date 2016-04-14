be470195d99f81b98e79ca22024963258eaf7b02
stanimir <sytolk@gmail.com>
2016-04-14 16:09:51 +0300

    initial commit

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Project can be compiled using java 7
 fermat-jooq-android:android-app **[generate]** task will generate java POJOs and DAOs classes from node.db
 
 fermat-jooq-android:android-app **[clean]** task will delete generated classes
+
+database node.db location: $projectDir/src/main/assets/databases/node.db
+
+###Test
+in onCreate method of the MainActivity have insert and fetchAll print node name in TextView
--- android-app/src/main/java/com/fermat/jooq/android/example/MainActivity.java
+++ android-app/src/main/java/com/fermat/jooq/android/example/MainActivity.java
@@ -2,6 +2,18 @@ package com.fermat.jooq.android.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import com.bitdubai.fermat.db.sqlite.generated.tables.daos.NodesCatalogDao;
+import com.bitdubai.fermat.db.sqlite.generated.tables.pojos.NodesCatalog;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "onCreate");
+
+        Connection connection = null;
+
+        try {
+            Class.forName("org.sqldroid.SQLDroidDriver").newInstance();
+// create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:" +
+                    new SQLiteAssetHelper(getApplicationContext(), "node.db", null, 12).getWritableDatabase().getPath());
+
+            DSLContext create = DSL.using(connection, SQLDialect.SQLITE);
+
+            NodesCatalog nodesCatalog = new NodesCatalog();
+            String publicKey = "mykey" + Math.random();
+            nodesCatalog.setIdentityPublicKey(publicKey);
+            nodesCatalog.setName("testNode" + Math.random());
+            nodesCatalog.setIp("192.168.1.1");
+            nodesCatalog.setDefaultPort(8080);
+            nodesCatalog.setLatitude((float) 42.34556);
+            nodesCatalog.setLongitude((float) 27.54556);
+            nodesCatalog.setRegisteredTimestamp((int) (System.currentTimeMillis() / 1000L));
+
+            NodesCatalogDao nodesCatalogDao = new NodesCatalogDao(create.configuration());
+            nodesCatalogDao.insert(nodesCatalog);
+
+
+            List<NodesCatalog> nodesCatalogArr = nodesCatalogDao.findAll();
+
+            if (nodesCatalogArr != null) {
+                Log.i("MainActivity", "nodesCatalogArr:"+nodesCatalogArr.size());
+
+                TextView textView = (TextView) findViewById(R.id.textid);
+                if (textView != null)
+                    for (NodesCatalog nodesCatalog1 : nodesCatalogArr) {
+                        textView.append(nodesCatalog1.getName() + ",");
+                    }
+            } else Log.i("MainActivity", "nodesCatalogArr:null");
+
+            connection.close();
+        } catch (Exception e) {
+            Log.e("MainActivity", "Ex:", e);
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (Exception ex) {
+                    Log.e(null, null, e);
+                }
+            }
+        }
     }
 }
